3bfb72e8fcb668402ad836f09d0254211f3b8bff
cainsusk <cain@susko.ca>
2024-02-15 00:28:37 -0500

    Add get-user endpoints
    Adds enpoints for getting all users and getting user by id.

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
 from typing import Dict
 from flask import Flask, render_template
 import db
+import users
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def display_data():    
+def display_data():
     tables = [table[0] for table in db.show_tables()]
 
     # Fetch all data from each table
@@ -14,6 +16,24 @@ def display_data():
         all_data[t] = db.select_table(t)
 
     return render_template('index.html', all_data=all_data)
+
+
+@app.route('/users')
+def get_users():
+    '''
+    Get All Users
+    '''
+    return users.get()
+
+
+@app.route('/users/<id>')
+def get_user(id):
+    '''
+    Get User By id
+    '''
+    user = users.get_dict().get(int(id))
+    return user if user else f"user with id:{id} was not found"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
