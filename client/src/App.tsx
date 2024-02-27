3943b59e3c81e86e6c725a2d80ce21a5ff2fbe46
Amanda Misek <amandamisek@Amandas-MacBook-Pro.local>
2024-02-27 13:18:42 -0500

    bottom padding

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="w-screen h-screen bg-white flex items-center px-4">
       <Navbar />
-      <div className="m-10 w-full h-[50%] max-w-screen-lg">
+      <div className="m-10 pb-10 w-full mt-96 max-w-screen-lg">
         <div className="ml-4 sm:ml-0 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-8">
           {cardSampleData.map((data) => (
             <Card key={data.id} data={data} />
