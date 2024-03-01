8e6fc1ea6524b4639d06e1db1da695b2416f3591
Amanda Misek <amandamisek@Amandas-MacBook-Pro.local>
2024-03-01 11:52:35 -0500

    admin cards

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,26 @@
 import Navbar from "./components/Navbar";
 import Modal from "./components/Modal";
+import AdminCards from "./components/AdminCards";
 
 export function App() {
+
+  const userRole = "admin";
+
+
   return (
-    <div className="w-screen h-screen bg-white flex items-center px-4">
-      <Navbar />
-      <Modal modalTitle="Add Item" buttonLabel="Save" secButtonLabel="Cancel"/>
+    <div className="w-screen h-screen bg-white flex px-4">
+      <div className="items-center ">
+        <Navbar />
+      </div>
+      <div className="flex flex-row">
+        <div className="pl-10 pt-24">
+          <AdminCards userRole={userRole} />
+        </div>
+        <div className="pt-32 pl-10">
+          <Modal modalTitle="Add Item" buttonLabel="Save" secButtonLabel="Cancel"/>
+        </div>
+      </div>
+      
     </div>
   );
 }
