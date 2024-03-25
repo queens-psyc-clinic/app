d218fbc388091d97aac35d917b94893da4da0141
Amanda Misek <amandamisek@Amandas-MacBook-Pro.local>
2024-03-24 22:17:00 -0400

    changed filters

--- client/src/components/Filter.tsx
+++ client/src/components/Filter.tsx
@@ -1,13 +1,13 @@
-import { ItemTypeOptions, LevelOfUse, Measure } from "../models/libraryItem";
+import { Measure, ItemTypeOptions, MaximumAge, MinimumAge } from "../models/libraryItem";
 import Dropdown from "./DropDown";
 
 const Filter = () => {
   return (
-    <div className="w-[40vw] flex flex-row items-center justify-center bg-gray-100 px-2 pb-1 rounded-full">
-      <Dropdown placeholder="Level" options={Object.values(LevelOfUse)} />
+    <div className="w-[45vw] flex flex-row items-center justify-center bg-gray-100 px-2 pb-1 rounded-full">
       <Dropdown placeholder="Measures" options={Object.values(Measure)} />
-
       <Dropdown placeholder="Item" options={ItemTypeOptions} />
+      <Dropdown placeholder="Min Age" options={MinimumAge} />
+      <Dropdown placeholder="Max Age" options={MaximumAge} />
     </div>
   );
 };
