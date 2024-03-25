22dd1eb9c954f8933a8197c0ab1e74bae50d42bd
victortarn <victor.tarnovski@gmail.com>
2024-03-25 09:05:55 -0400

    Update item controller to work with new db schema and added post method to get data with filters

--- app/resources/item.py
+++ app/resources/item.py
@@ -6,15 +6,16 @@ from common.db import execute_query, execute_sql_query, select_table, check_exis
 
 item_fields = {
     'ID': fields.String,
-    'Status': fields.String,
+    'Status': fields.Integer,
     'ItemType': fields.String,
     'ItemName': fields.String,
     'EditionNumber': fields.String,
     'Ages': fields.String,
     'NumberOfParts': fields.String,
     'Location': fields.String,
-    'OrderingCompany': fields.String,
     'TestID': fields.String,
+    'IsArchived': fields.Integer,
+    'Stock': fields.Integer
 }
 
 class Item(Resource):
@@ -45,12 +46,37 @@ class Item(Resource):
             type: object
             properties:
               ID:
-                type: integer
+                type: string
                 description: The item ID.
+              Status:
+                type: integer
+                description: Item status
+              ItemType:
+                type: string
+                description: Item type
               ItemName:
                 type: string
                 description: The name of the item.
-              # Add other properties here
+              EditionNumber:
+                type: string
+                description: The edition number
+                type: string
+                description: The intended ages
+              NumberOfParts:
+                type: string
+                description: The number of parts
+              Location:
+                type: string
+                description: The item location
+              TestID:
+                type: string
+                description: The related testID
+              IsArchived:
+                type: integer
+                description: If the item is archived
+              Stock:
+                type: integer
+                description: The number of stock
         """
         return _select_with_id(id)
     
@@ -75,17 +101,6 @@ class Item(Resource):
                 $ref: '#/definitions/Item'
           500:
             description: Error fetching items
-        definitions:
-          Item:
-            type: object
-            properties:
-              ID:
-                type: integer
-                description: The item ID.
-              ItemName:
-                type: string
-                description: The name of the item.
-              # Add other properties here
         """
         return _delete_by_id(id)
 
--- app/resources/items.py
+++ app/resources/items.py
@@ -5,6 +5,16 @@ from flask_restful import Resource, abort, marshal_with, reqparse, request, fiel
 from common.db import execute_sql_query, select_table, check_exists, execute_query
 from resources.item import item_fields
 
+# for getting args not in BODY or PATH
+items_parser = reqparse.RequestParser()
+# define the args to look for
+items_parser.add_argument(
+    'columns', dest='columns',
+    location='args', action='append',
+    help='The columns to select'
+)
+
+
 class Items(Resource):
 
     @marshal_with(item_fields)
@@ -23,88 +33,159 @@ class Items(Resource):
                 $ref: '#/definitions/Item'
           500:
             description: Error fetching items
-        definitions:
-          Item:
-            type: object
-            properties:
-              ID:
-                type: integer
-                description: The item ID.
-              ItemName:
-                type: string
-                description: The name of the item.
-              # Add other properties here
         """
 
         return select_table('Items')
 
     @marshal_with(item_fields)
-    def post(self, test_ID, item_type="", item_name="", edition_number="", ages="", 
-            number_of_parts="", location="", ordering_company=""):
+    def post(self):
         """
-        This endpoint adds a new item
+        Get (optional) columns from users satisfying filters
         ---
         tags:
           - Items
+        requestBody:
+          content:
+            application/json:
+              schema:
+                id: User
         parameters:
-          - name: item_type
-            in: formData
-            type: string
-            required: false
-            description: The type of the item
-          - name: item_name
-            in: formData
-            type: string
-            required: false
-            description: The name of the item
-          - name: edition_number
-            in: formData
-            type: string
-            required: false
-            description: The edition number of the item, as a string
-          - name: ages
-            in: formData
-            type: string
-            required: false
-            description: The recommended ages for the item, as a string
-          - name: number_of_parts
-            in: formData
-            type: string
-            required: false
-            description: The number of parts the item contains, as a string
-          - name: location
-            in: formData
-            type: string
-            required: false
-            description: The location of the item
-          - name: ordering_company
-            in: formData
-            type: string
-            required: false
-            description: The company ordering the item
-          - name: test_ID
-            in: query
-            type: string
+          - in: body
+            name: filters
+            description: the filters and columns to retrive
+            schema:
+              id: Item
             required: true
-            description: The test ID associated with the item, as a string
+          - in: query
+            name: columns
+            description: columns to select from db
+            type: array
+            items:
+              type: string
+            uniqueItems: true
         responses:
-          200:
-            description: Item successfully added
-          400:
-            description: Error in adding the item
+          201:
+            description: A list of items
+            schema:
+              id: Item
+              properties:
+                ID:
+                  type: string
+                  description: The item ID.
+                Status:
+                  type: integer
+                  description: Item status
+                ItemType:
+                  type: string
+                  description: Item type
+                ItemName:
+                  type: string
+                  description: The name of the item.
+                EditionNumber:
+                  type: string
+                  description: The edition number
+                  type: string
+                  description: The intended ages
+                NumberOfParts:
+                  type: string
+                  description: The number of parts
+                Location:
+                  type: string
+                  description: The item location
+                TestID:
+                  type: string
+                  description: The related testID
+                IsArchived:
+                  type: integer
+                  description: If the item is archived
+                Stock:
+                  type: integer
+                  description: The number of stock
+          403:
+            description: bad request
+        example:
         """
-        return _post_item(item_type, item_name, edition_number, ages, 
-                          number_of_parts, location, ordering_company, test_ID)
 
-def _post_item(item_type, item_name, edition_number, ages, 
-             number_of_parts, location, ordering_company, test_ID): 
-    return execute_query("""INSERT INTO Items (ID, Status, ItemType, ItemName,
-                          EditionNumber, Ages, NumberOfParts, Location, 
-                         OrderingCompany, TestID) 
-                         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) """, 
-                         [(_generate_unique_id(), "1", item_type, item_name, 
-                           edition_number, ages, number_of_parts, location, 
-                           ordering_company, test_ID)], True)
+        # get data from *not* BODY or PATH
+        columns = items_parser.parse_args()['columns']
+        # get data from BODY
+        filters = request.get_json()
+
+        return _select_cols(filters, columns), 201
+
+
+#     @marshal_with(item_fields)
+#     def post(self, test_ID, item_type="", item_name="", edition_number="", ages="", 
+#             number_of_parts="", location="", ordering_company=""):
+#         """
+#         This endpoint adds a new item
+#         ---
+#         tags:
+#           - Items
+#         parameters:
+#           - name: item_type
+#             in: formData
+#             type: string
+#             required: false
+#             description: The type of the item
+#           - name: item_name
+#             in: formData
+#             type: string
+#             required: false
+#             description: The name of the item
+#           - name: edition_number
+#             in: formData
+#             type: string
+#             required: false
+#             description: The edition number of the item, as a string
+#           - name: ages
+#             in: formData
+#             type: string
+#             required: false
+#             description: The recommended ages for the item, as a string
+#           - name: number_of_parts
+#             in: formData
+#             type: string
+#             required: false
+#             description: The number of parts the item contains, as a string
+#           - name: location
+#             in: formData
+#             type: string
+#             required: false
+#             description: The location of the item
+#           - name: ordering_company
+#             in: formData
+#             type: string
+#             required: false
+#             description: The company ordering the item
+#           - name: test_ID
+#             in: query
+#             type: string
+#             required: true
+#             description: The test ID associated with the item, as a string
+#         responses:
+#           200:
+#             description: Item successfully added
+#           400:
+#             description: Error in adding the item
+#         """
+#         return _post_item(item_type, item_name, edition_number, ages, 
+#                           number_of_parts, location, ordering_company, test_ID)
+
+# def _post_item(item_type, item_name, edition_number, ages, 
+#              number_of_parts, location, ordering_company, test_ID): 
+#     return execute_query("""INSERT INTO Items (ID, Status, ItemType, ItemName,
+#                           EditionNumber, Ages, NumberOfParts, Location, 
+#                          OrderingCompany, TestID) 
+#                          VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) """, 
+#                          [(_generate_unique_id(), "1", item_type, item_name, 
+#                            edition_number, ages, number_of_parts, location, 
+#                            ordering_company, test_ID)], True)
+    
+
+def _select_cols(cn, cl): return execute_sql_query(
+    "SELECT", "Items", conditions=cn, columns=cl)
+
 def _generate_unique_id(): 
 
     existing_ids = execute_query("SELECT `ID` FROM `Items`", None, False)
