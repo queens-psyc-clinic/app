96e3a548d2ebd008d81ae4e726893b3b5be2a694
Farah Amer <farahamer@Farahs-MacBook-Pro-2.local>
2024-04-03 22:18:45 -0400

    just created clear filters button

--- client/src/components/Filter.tsx
+++ client/src/components/Filter.tsx
@@ -18,20 +18,25 @@ interface FilterProps {
 const Filter: React.FC<FilterProps> = ({ placeholders, options, onChange }) => {
   const [filter, setFilter] = useState({});
   return (
-    <div className="w-max flex flex-row items-center justify-center bg-gray-100 px-2 pb-1 rounded-full">
-      {placeholders.map((placeholder, index) => (
-        <Dropdown
-          key={index}
-          placeholder={placeholder}
-          options={options[index].map(String)}
-          onChange={(option: string, label: string) => {
-            setFilter({ ...filter, [placeholder]: option });
-            if (onChange) {
-              onChange({ ...filter, [placeholder]: option });
-            }
-          }}
-        />
-      ))}
+    <div className="flex h-min">
+      <div className="w-max flex flex-row items-center justify-center bg-gray-100 px-2 pb-1 rounded-full">
+        {placeholders.map((placeholder, index) => (
+          <Dropdown
+            key={index}
+            placeholder={placeholder}
+            options={options[index].map(String)}
+            onChange={(option: string, label: string) => {
+              setFilter({ ...filter, [placeholder]: option });
+              if (onChange) {
+                onChange({ ...filter, [placeholder]: option });
+              }
+            }}
+          />
+        ))}
+      </div>
+      <button className="h-auto ml-2 rounded-full border boder-black px-4 py-2  text-xs">
+        Clear Filters
+      </button>
     </div>
   );
 };
